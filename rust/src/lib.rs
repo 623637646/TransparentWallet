4360449514e42f648a7435b2f1f258c3815fe79b
Yanni Wang <wy19900729@gmail.com>
2025-11-06 18:16:36 +0800

    [BugFix] Relocate init_app function because it's not called.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,3 @@
 pub mod api;
 mod frb_generated;
 mod rx_helper;
-
-#[flutter_rust_bridge::frb(init)]
-pub fn init_app() {
-    // Default utilities - feel free to customize
-    flutter_rust_bridge::setup_default_user_utils();
-}
